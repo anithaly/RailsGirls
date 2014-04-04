a3b8156c2d4bd9147a5daec718e82eab1fa05cf5
Natalia Stanko <anithaly@gmail.com>
2014-04-05 01:52:43 +0200

    idea validation

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,3 +1,11 @@
 class Idea < ActiveRecord::Base
   mount_uploader :picture, PictureUploader
+
+  validates :name,
+    length: {
+      minimum: 2,
+      too_short: "must have at least %{count} words"
+    },
+    :presence => {:message => "Enter the idea"}
+  validates :description, presence: true
 end
