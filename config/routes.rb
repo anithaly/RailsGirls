67cfa7552f733db7e6b04a9e0cd4fef7ba021981
Natalia Stanko <anithaly@gmail.com>
2014-04-05 01:44:59 +0200

    uploading pictures by carrierwave

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Railsgirls::Application.routes.draw do
   get "pages/info"
   resources :ideas
-  root :to => redirect('/ideas')
+  # root :to => redirect('/ideas')
+  root 'ideas#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
